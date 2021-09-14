93bf2b6caa75878877dfddcdfac37758d8e85a8d
U-BSD\412710 <412710@SHS-10D9C06F2.beaverton.k12.or.us>
2021-09-14 12:22:20 -0700

    First Commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
+#include <iostream>//including packages to use everythign in them
 #include <string>
-using namespace std;
+using namespace std;//Without this you have to write std::cout
 
-int main() {
-  while (true) {
-    srand(time(NULL)); 
-    cout << "Hey there! This is the guessing game game! The computer here will choose a number from 1-100, and you have to guess it!";
-    int num = rand()% 100 + 1;   
-    int count = 0;
-    while (true) {
+int main() {//the function
+  int a = 0;//for while loop
+  while (a == 0) {
+    srand(time(NULL)); //Setting random seed
+    cout << "Hey there! This is the guessing game game! The computer here will choose a number from 1-100, and you have to guess it!";//printing opening message
+    int num = rand()% 100 + 1; //generating random num  
+    int count = 0;//the count of guesses
+    int b = 0;//another variable for while loop. A and b were used to break the while loops when i wanted. 
+    while (b == 0) {//
       int guess;
       cin >> guess;
       if (guess == num) {
@@ -18,11 +20,12 @@ int main() {
         cout << "Want to play again?";
         string x;
         cin >> x;
-      if (x == "y") {
+      if (x == "yes") {
         continue;
       }
       else {
-        break;
+        a = 1;
+        b = 1;
       }
 
       }
@@ -40,5 +43,3 @@ int main() {
   }
   return 0;
 }
-
-    
